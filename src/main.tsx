aa0d97213fe2a84df99ea8d863347636ddf9888d
snowingfox <1503401882@qq.com>
2024-05-31 22:09:06 +0800

    feat: init

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './router.tsx'
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+    },
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={routes} />
+    </QueryClientProvider>
+  </React.StrictMode>,
+)
